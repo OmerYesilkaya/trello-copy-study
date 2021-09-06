68568d5b3d4fa02772ae37f67ac4b33aac5625b4
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-09-06 19:10:08 +0300

    fix typo

--- src/components/Navbar/NavbarButton.tsx
+++ src/components/Navbar/NavbarButton.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@chakra-ui/button";
 
-type NavbarButtonTypes = {
+type NavbarButtonProps = {
 	children: JSX.Element;
 };
 
-export default function NavbarButton({ children }: NavbarButtonTypes) {
+export default function NavbarButton({ children }: NavbarButtonProps) {
 	return (
 		<Button colorScheme="twitter" h="30px" minW="30px" p="0px" mr="5px">
 			{children}
