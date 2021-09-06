902652bd2ed6441a46025eb6601791acc4b45bbe
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-09-06 17:31:41 +0300

    create home page skeleton

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Flex } from "@chakra-ui/layout";
+
+export default function Home() {
+	return (
+		<Flex w="55%" mt="2em">
+			home
+			{/* sidebar */}
+			{/* active projects */}
+		</Flex>
+	);
+}
