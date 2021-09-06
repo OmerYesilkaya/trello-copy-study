53f26780a68520b58531bb506f3d5cfd2667c859
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-09-06 19:09:44 +0300

    add created components to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import { Flex } from "@chakra-ui/layout";
+import Boards from "components/Home/Boards";
+import SideBar from "components/Home/SideBar";
 
 export default function Home() {
 	return (
 		<Flex w="55%" mt="2em">
-			home
-			{/* sidebar */}
-			{/* active projects */}
+			<SideBar />
+			<Boards />
 		</Flex>
 	);
 }
