cdc16963d59a77b04084ac5be125cfce3d58b902
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-09-06 19:09:30 +0300

    create motion chakra components for ease of use

---
+++ src/motion/chakra.ts
@@ -0,0 +1,7 @@
+import { Image } from "@chakra-ui/image";
+import { Box, Flex } from "@chakra-ui/layout";
+import { motion } from "framer-motion";
+
+export const MotionFlex = motion(Flex);
+export const MotionBox = motion(Box);
+export const MotionImage = motion(Image);
