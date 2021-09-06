f011ebe7da77c98a1def9082e3b47ee7e90471cd
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-09-06 19:10:34 +0300

    update trello logo hover visuals

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-import { Image } from "@chakra-ui/image";
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
 import { Box, Flex, Text } from "@chakra-ui/layout";
 import { colors } from "constants/colors";
@@ -8,6 +7,7 @@ import { FiInfo } from "react-icons/fi";
 import NavbarButton from "./NavbarButton";
 import trelloLogo from "assets/trello_logo.gif";
 import PlaceholderUserLogo from "components/PlaceholderUserLogo";
+import { MotionImage } from "motion/chakra";
 
 export default function Navbar() {
 	return (
@@ -33,8 +33,8 @@ export default function Navbar() {
 				</InputGroup>
 			</Flex>
 			{/* 40 is about the same with half of the trello logo width, it is hardcoded, maybe get image width with ref in the future */}
-			<Box position="absolute" left={window.innerWidth / 2 - 40}>
-				<Image h="15px" src={trelloLogo} opacity={0.6} />
+			<Box cursor="pointer" position="absolute" left={window.innerWidth / 2 - 40}>
+				<MotionImage opacity={0.6} whileHover={{ opacity: 1 }} h="15px" src={trelloLogo} />
 			</Box>
 			<Flex align="center">
 				<NavbarButton>
