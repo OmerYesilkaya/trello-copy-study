adb6548b9d470b1a30461059fe652473bd9c16cc
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-09-07 17:12:50 +0300

    create BoardDetails page

---
+++ src/pages/BoardDetails.tsx
@@ -0,0 +1,34 @@
+import { Flex } from "@chakra-ui/layout";
+import BoardNavbar from "components/boardDetails/BoardNavbar";
+import Lists from "components/boardDetails/Lists";
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import { useBoardStore } from "store/useBoardStore";
+
+type BoardParams = {
+	id: string;
+};
+
+export default function BoardDetails() {
+	const { id } = useParams<BoardParams>();
+	const { getBoardData, setActiveBoardId, getCurrentTheme } = useBoardStore((state) => ({
+		getBoardData: state.getBoardData,
+		setActiveBoardId: state.setActiveBoardId,
+		getCurrentTheme: state.getCurrentTheme,
+	}));
+
+	const boardData = getBoardData(id);
+
+	useEffect(() => {
+		setActiveBoardId(id);
+	}, [setActiveBoardId, id]);
+
+	if (!boardData) return null;
+
+	return (
+		<Flex direction="column" bg={`${getCurrentTheme()}.400`} w="100%" h="100%">
+			<BoardNavbar data={boardData} />
+			<Lists data={boardData} />
+		</Flex>
+	);
+}
