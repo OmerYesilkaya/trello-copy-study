9e3b1920b98ee00de20ff2d8502995ffd72c2a5a
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-09-07 17:11:18 +0300

    create dummy users data

---
+++ src/constants/users.ts
@@ -0,0 +1,9 @@
+import { User } from "models/User";
+
+export const users: User[] = [
+	{ name: "Ömer Yeşilkaya", id: "da3c858292b0" },
+	{ name: "İskender Kartal", id: "74818bfef56b" },
+	{ name: "Sevgi Marangoz", id: "27b5cd1106eb" },
+	{ name: "Muhsin Peynirci", id: "e3e7fbdbf524" },
+	{ name: "Hamit Bardakçı", id: "8be4e0b10a13" },
+];
