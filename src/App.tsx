1fc3a2e0ef3048209743fad313085f90afc33ad4
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-09-07 17:07:25 +0300

    include navbar in router so that it is possible to route user from navbar buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import { Flex } from "@chakra-ui/layout";
-import Navbar from "components/Navbar/Navbar";
+import Navbar from "components/navbar/Navbar";
+import BoardDetails from "pages/BoardDetails";
 import Home from "pages/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
 	return (
-		<Flex w="100vw" h="100vh" direction="column" align="center">
-			<Navbar />
-			<Router>
+		<Router>
+			<Flex w="100vw" h="100vh" direction="column" align="center">
+				<Navbar />
 				<Switch>
+					<Route exact path="/board/:id">
+						<BoardDetails />
+					</Route>
 					<Route path="/">
 						<Home />
 					</Route>
 				</Switch>
-			</Router>
-		</Flex>
+			</Flex>
+		</Router>
 	);
 }
 
