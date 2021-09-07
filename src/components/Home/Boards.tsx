64b69aee8662b3d132eb43fc5a3324ffb765acb5
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-09-07 17:09:39 +0300

    update home page with new board cards

--- src/components/Home/Boards.tsx
+++ src/components/Home/Boards.tsx
@@ -1,28 +1,42 @@
 import { Button } from "@chakra-ui/button";
-import { Flex, Text } from "@chakra-ui/layout";
+import { useDisclosure } from "@chakra-ui/hooks";
+import { Flex, Grid, Text } from "@chakra-ui/layout";
 import { FaPlus } from "react-icons/fa";
+import { useBoardStore } from "store/useBoardStore";
+import BoardCard from "./BoardCard";
+import CreateBoardModal from "./CreateBoardModal";
 
 export default function Boards() {
-	function handleCreateBoard() {
-		// TODO(omer): open up a modal asking board name and other minor stuff (theme color, people to add, etc..)
-	}
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const { boards } = useBoardStore((state) => ({ boards: state.boards }));
 
 	return (
-		<Flex
-			fontSize="14px"
-			border="3px dashed"
-			borderColor="#E4F0F6"
-			p="1em"
-			mx="1em"
-			borderRadius="2px"
-			w="100%"
-			direction="column"
-			align="center"
-		>
-			<Text>You currently dont have any active boards, click below to create one</Text>
-			<Button colorScheme="twitter" mt="auto" fontSize="14px" onClick={handleCreateBoard}>
-				<Text mr="4px">Create Board</Text> <FaPlus size="0.8em" />
+		<Flex w="100%" direction="column" mx="1em">
+			<Flex
+				fontSize="14px"
+				border="3px dashed"
+				borderColor="#E4F0F6"
+				p="1em"
+				mb="1em"
+				borderRadius="2px"
+				w="100%"
+				direction="column"
+				align="center"
+			>
+				{boards.length === 0 ? (
+					<Text>Aktif panonuz bulunmamaktadır. Aşağıdaki butona basıp oluşturabilirsiniz.</Text>
+				) : (
+					<Grid w="100%" templateColumns="repeat(4,1fr)" gap="10px" placeItems="center">
+						{boards.map((board) => (
+							<BoardCard key={board.id} board={board} />
+						))}
+					</Grid>
+				)}
+			</Flex>
+			<Button w="max-content" mx="auto" colorScheme="twitter" mt="auto" fontSize="14px" onClick={onOpen}>
+				<Text mr="4px">Pano Oluştur</Text> <FaPlus size="0.8em" />
 			</Button>
+			<CreateBoardModal isOpen={isOpen} onClose={onClose} />
 		</Flex>
 	);
 }
