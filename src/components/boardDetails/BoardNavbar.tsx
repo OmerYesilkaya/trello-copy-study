2ee8bd9a03a177ec51582a0c03b46920727cb442
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-09-07 17:07:46 +0300

    create sub-navbar for board details page

---
+++ src/components/boardDetails/BoardNavbar.tsx
@@ -0,0 +1,44 @@
+import { Button, IconButton } from "@chakra-ui/button";
+import { Divider, Flex, Text } from "@chakra-ui/layout";
+import UserLogo from "components/common/UserLogo";
+import { Board } from "models/Board";
+import { TiStar, TiStarOutline } from "react-icons/ti";
+import { useBoardStore } from "store/useBoardStore";
+
+type BoardNavbarParams = {
+	data: Board;
+};
+
+export default function BoardNavbar({ data }: BoardNavbarParams) {
+	const { toggleFavoriteBoard } = useBoardStore((state) => ({ toggleFavoriteBoard: state.toggleFavoriteBoard }));
+	return (
+		<Flex align="center" w="100%" p="0.5em" px="1em" color="white">
+			<Text fontSize="20px" fontWeight="bold" mr="0.5em">
+				{data.name}
+			</Text>
+			<IconButton
+				size="sm"
+				icon={data.isFavorited ? <TiStar size="1.42em" /> : <TiStarOutline size="1.42em" />}
+				aria-label="Favorite Board"
+				colorScheme="whiteAlpha"
+				mr="0.5em"
+				onClick={() => toggleFavoriteBoard(data.id)}
+			/>
+			<Divider orientation="vertical" h="50%" mr="0.5em" />
+			<Button colorScheme="whiteAlpha" size="sm" mr="0.5em">
+				Çalışma alanı ekle
+			</Button>
+			<Divider orientation="vertical" h="50%" mr="0.5em" />
+			<Button colorScheme="whiteAlpha" size="sm" mr="0.5em">
+				Görünürlüğü değiştir
+			</Button>
+			<Divider orientation="vertical" h="50%" mr="0.5em" />
+			{data.assignees.map((assignee) => (
+				<UserLogo name={assignee.name} />
+			))}
+			<Button colorScheme="whiteAlpha" size="sm" mr="0.5em">
+				Davet et
+			</Button>
+		</Flex>
+	);
+}
