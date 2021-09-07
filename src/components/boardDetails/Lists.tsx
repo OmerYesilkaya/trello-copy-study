546622075fd64815fb4ec7658dcf1a5a8c7470e6
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-09-07 21:14:09 +0300

    map each list in board and add create list element at the end

--- src/components/boardDetails/Lists.tsx
+++ src/components/boardDetails/Lists.tsx
@@ -1,16 +1,37 @@
-import { Text } from "@chakra-ui/layout";
+import { Flex } from "@chakra-ui/layout";
 import { Board } from "models/Board";
+import { useEffect, useRef, useState } from "react";
+import CreateListElement from "./CreateListElement";
+import ListElement from "./ListElement";
 
 type ListsProps = {
-	data: Board;
+	board: Board;
 };
 
-export default function Lists({ data }: ListsProps) {
+export default function Lists({ board }: ListsProps) {
+	const [isEditActive, setIsEditActive] = useState<string | null>(null);
+	const targetRef = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		// check wheter mouse click is on create list element, if it is not close edit segment
+		if (!targetRef.current) return;
+		function handleMouseDown(e: MouseEvent) {
+			if (targetRef.current && targetRef.current.contains(e.target as any)) return;
+			setIsEditActive(null);
+		}
+
+		window.addEventListener("mousedown", handleMouseDown);
+		return () => {
+			window.removeEventListener("mousedown", handleMouseDown);
+		};
+	}, []);
+
 	return (
-		<>
-			{data.lists.forEach((list) => (
-				<Text key={list.id}>{list.name}</Text>
+		<Flex p="1em" maxW="100vw" overflow="auto" h="100%">
+			{board.lists.map((list) => (
+				<ListElement key={list.id} list={list} />
 			))}
-		</>
+			<CreateListElement targetRef={targetRef} isEditActive={isEditActive} setIsEditActive={setIsEditActive} />
+		</Flex>
 	);
 }
