6d08c238d3514b6455248565f526084c99bbfcea
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-09-07 17:12:33 +0300

    add MotionCenter component

--- src/motion/chakra.ts
+++ src/motion/chakra.ts
@@ -1,7 +1,8 @@
 import { Image } from "@chakra-ui/image";
-import { Box, Flex } from "@chakra-ui/layout";
+import { Box, Center, Flex } from "@chakra-ui/layout";
 import { motion } from "framer-motion";
 
 export const MotionFlex = motion(Flex);
 export const MotionBox = motion(Box);
 export const MotionImage = motion(Image);
+export const MotionCenter = motion(Center);
