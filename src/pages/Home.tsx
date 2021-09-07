8be04576b4c5c19708acc15cd4f93649150332f6
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-09-07 17:13:12 +0300

    remove color theme on home page routing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,16 @@
 import { Flex } from "@chakra-ui/layout";
-import Boards from "components/Home/Boards";
-import SideBar from "components/Home/SideBar";
+import Boards from "components/home/Boards";
+import SideBar from "components/home/SideBar";
+import { useEffect } from "react";
+import { useBoardStore } from "store/useBoardStore";
 
 export default function Home() {
+	const { setActiveBoardId } = useBoardStore((state) => ({ setActiveBoardId: state.setActiveBoardId }));
+
+	useEffect(() => {
+		setActiveBoardId(null);
+	}, [setActiveBoardId]);
+
 	return (
 		<Flex w="55%" mt="2em">
 			<SideBar />
