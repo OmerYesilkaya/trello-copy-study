89ccb916868e9f8a7dcbe06fed9a8f9859911667
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-09-09 17:27:18 +0300

    fix long list over flow problem

--- src/components/card/Cards.tsx
+++ src/components/card/Cards.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@chakra-ui/layout";
 import { List } from "models/List";
 import { Draggable } from "react-beautiful-dnd";
 import CardElement from "./CardElement";
@@ -8,12 +9,12 @@ type Props = {
 
 export default function Cards({ list }: Props) {
 	return (
-		<>
+		<Box maxH="60vh" overflow="auto">
 			{list.cards.map((card, index) => (
 				<Draggable key={card.id} draggableId={card.id} index={index}>
 					{(provided, snapshot) => <CardElement key={card.id} card={card} provided={provided} isDragging={snapshot.isDragging} />}
 				</Draggable>
 			))}
-		</>
+		</Box>
 	);
 }
