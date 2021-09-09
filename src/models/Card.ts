474d5185c9288590ac0a5509ea38a01028c13234
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-09-09 17:28:14 +0300

    add tag and color types to card

--- src/models/Card.ts
+++ src/models/Card.ts
@@ -1,13 +1,15 @@
 import { Comment } from "./Comment";
+import { Tag } from "./Tag";
 import { User } from "./User";
 
 export type Card = {
 	id: string;
 	name: string;
-	thumbnail: string;
+	color: string;
+	tags: Tag[];
+	assignees: User[];
 	description: string;
 	comments: Comment[];
 	isFollowed: boolean;
-	assignees: User[];
 	parentListId: string;
 };
