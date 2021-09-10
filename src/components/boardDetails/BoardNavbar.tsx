ec3782c9f56ec0e0ee56bcd3a3895b1ef5a55cee
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-09-10 14:09:43 +0300

    move search bar to board navbar

--- src/components/boardDetails/BoardNavbar.tsx
+++ src/components/boardDetails/BoardNavbar.tsx
@@ -1,7 +1,10 @@
 import { Button, IconButton } from "@chakra-ui/button";
-import { Divider, Flex, Text } from "@chakra-ui/layout";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { Divider, Flex, HStack, Text } from "@chakra-ui/layout";
 import UserLogo from "components/common/UserLogo";
 import { Board } from "models/Board";
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 import { TiStar, TiStarOutline } from "react-icons/ti";
 import { useBoardStore } from "store/useBoardStore";
 
@@ -10,7 +13,18 @@ type BoardNavbarParams = {
 };
 
 export default function BoardNavbar({ data }: BoardNavbarParams) {
-	const { toggleFavoriteBoard } = useBoardStore((state) => ({ toggleFavoriteBoard: state.toggleFavoriteBoard }));
+	const { toggleFavoriteBoard, getCurrentTheme, currentSearchFilter, setCurrentSearchFilter } = useBoardStore((state) => ({
+		toggleFavoriteBoard: state.toggleFavoriteBoard,
+		getCurrentTheme: state.getCurrentTheme,
+		currentSearchFilter: state.currentSearchFilter,
+		setCurrentSearchFilter: state.setCurrentSearchFilter,
+	}));
+	const [searchValue, setSearchValue] = useState("");
+
+	function handleSearch() {
+		setCurrentSearchFilter(searchValue);
+	}
+
 	return (
 		<Flex minH="4.5vh" align="center" w="100%" px="0.5em" color="white">
 			<Text fontSize="20px" fontWeight="bold" mr="0.5em">
@@ -36,9 +50,42 @@ export default function BoardNavbar({ data }: BoardNavbarParams) {
 			{data.assignees.map((assignee) => (
 				<UserLogo key={assignee.id} name={assignee.name} />
 			))}
+
 			<Button colorScheme="whiteAlpha" size="sm" mr="0.5em">
 				Davet et
 			</Button>
+			<Divider orientation="vertical" h="50%" mr="0.5em" />
+
+			<HStack spacing="5px">
+				<InputGroup size="sm" w="200px">
+					<Input
+						border="none"
+						fontSize="14px"
+						value={searchValue}
+						placeholder="Panoda ara..."
+						bg={`${getCurrentTheme()}.600`}
+						onChange={(e) => setSearchValue(e.target.value)}
+					/>
+					<InputRightElement children={<FaSearch color="white" />} />
+				</InputGroup>
+				{searchValue && (
+					<Button size="sm" colorScheme={getCurrentTheme()} onClick={handleSearch}>
+						Ara
+					</Button>
+				)}
+				{currentSearchFilter && (
+					<Button
+						size="sm"
+						colorScheme="red"
+						onClick={() => {
+							setCurrentSearchFilter("");
+							setSearchValue("");
+						}}
+					>
+						Filtreyi kaldır
+					</Button>
+				)}
+			</HStack>
 		</Flex>
 	);
 }
